140dc5538e6e4dc063cd4805c4c33240a6e01757
04maryam <itsmaryamali54@gmail.com>
2025-02-17 22:34:07 +0300

    -

--- src/WorldClock.jsx
+++ src/WorldClock.jsx
@@ -2,6 +2,14 @@ import Clock from "./Clock.jsx";
 // import "./App.css";
 
 export default function WorldClock({ clockData }) {
+  if (!clockData || clockData.length === 0) {
+    return (
+      <p className="d-flex justify-content-center fs-6 my-5">
+        No clocks to display.
+      </p>
+    );
+  }
+
   return (
     <div className="d-md-flex flex-wrap w-100 my-md-3 ps-md-3">
       {clockData.map((timeZone, index) => (
